0b51d78201573e02fe26dc885131c54c2bb45482
NguyenDoan85 <ericdoan2008@gmail.com>
2022-09-22 01:07:02 -0700

    refactor

--- models/Thought.js
+++ models/Thought.js
@@ -3,35 +3,6 @@ const { Schema, model, Types } = require('mongoose');
 // import moment module to format the timestamp 
 const moment = require('moment')
 
-// thought schema
-const thoughtSchema = new Schema (
-    {
-      thoughtText: {
-        type: String,
-        required: true,
-        minlength: 1,
-        maxlength: 280,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-      },
-      username: {
-        type: String,
-        required: true,
-      },
-      reactions: [reactionSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-);
-
 //reaction schema
 const reactionSchema = new Schema (
     {
@@ -61,7 +32,36 @@ const reactionSchema = new Schema (
         },
         id: false,
     }
-);
+)
+
+// thought schema
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
 
 // get total count of friends
 thoughtSchema.virtual('reactionCount')
