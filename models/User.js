199c05e483c95b3aa0ab266753bb5e471cca2d61
NguyenDoan85 <ericdoan2008@gmail.com>
2022-09-21 20:43:45 -0700

    adding model schema for user and export it

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model, Types } = require('mongoose');
+
+// create model schema for user
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+// create instance for friendCount
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+// create the User model using the UserSchema
+const User = model('User', userSchema);
+// export the User model
+module.exports = User;
